89866c4fe9097e43e5e17fcc4d7e7ecd4ef3c85f
Nick <nickolas.lubelczyk@mymail.champlain.edu>
2025-09-22 02:45:30 -0400

    week4

--- week4/main.ps1
+++ week4/main.ps1
@@ -1,8 +1,13 @@
 ﻿. (Join-Path $PScriptRoot Apache-Logs.ps1)
+. (Join-Path $PScriptRoot ApacheLogs1.ps1)
 
 clear
 
-$ips = Get-ApacheLogIPs -Page "page1.html" -HttpCode "200" -Browser "Chrome"
+$ips = Get-ApacheLogIPs -Page "index.html" -HttpCode "404" -Browser "Chrome"
+
 
 $counts = $ips.IP | Group-Object
 $counts | Select-Object Count, Name
+
+$tableRecords = ApacheLogs1
+$tableRecords | Format-Table -AutoSize -Wrap
